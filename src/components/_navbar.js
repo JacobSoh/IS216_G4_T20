f4066656e21a20d2e7ff02924e1ca7c6fdaad885
Jacob Soh <developer.swr.2001@gmail.com>
2025-09-27 20:05:25 +0800

    update image

--- src/components/_navbar.js
+++ src/components/_navbar.js
@@ -3,6 +3,7 @@
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 
 const navigation = [
@@ -85,7 +86,7 @@ export default function Navbar() {
 							<MenuButton className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
 								<span className="absolute -inset-1.5" />
 								<span className="sr-only">Open user menu</span>
-								<img
+								<Image 
 									alt=""
 									src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
 									className="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10"
