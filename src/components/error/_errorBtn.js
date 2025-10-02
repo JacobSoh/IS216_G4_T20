24250233567e36a6d222e63d65ab8e567251c7d2
Jacob Soh <developer.swr.2001@gmail.com>
2025-10-02 08:49:34 +0800

    Update Error UI

---
+++ src/components/error/_errorBtn.js
@@ -0,0 +1,27 @@
+"use client";
+import { useRouter } from 'next/navigation';
+
+export default function _errorBtn() {
+    const router = useRouter();
+
+    const goBack = () => {
+        router.push('/');
+    };
+    return (
+        <button
+            type="button"
+            className={`
+                lg:text-xl lg:py-3 lg:px-5
+                md:text-md md:px-4
+                text-sm py-2 px-3
+                text-black bg-(--custom-cream-yellow) 
+                hover:bg-(--custom-cream-yellow-darker) 
+                focus:ring-4 focus:ring-yellow-500 
+                rounded-lg focus:outline-none    
+            `}
+            onClick={goBack}
+        >
+            Back to Home
+        </button>
+    );
+};
---
+++ src/components/error/index.js
@@ -0,0 +1,3 @@
+export { default as _eyes } from "@/components/error/_eyes";
+export { default as _errorMsg } from "@/components/error/_errorMsg";
+export { default as _errorBtn } from "@/components/error/_errorBtn";
