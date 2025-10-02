c9227d98cce2ab85868113de0b162dc67b719327
Jacob Soh <developer.swr.2001@gmail.com>
2025-10-02 08:49:34 +0800

    Update Error UI

--- src/components/_error.js
+++ src/components/_error.js
@@ -1,38 +1,20 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import _eyes from "@/components/error/_eyes";
+import {
+    _eyes,
+    _errorMsg,
+    _errorBtn
+} from '@/components/error/index';
 
 export default function Error({ status, error, reset }) {
-    const router = useRouter();
-
-    const goBack = () => {
-        if (typeof window !== 'undefined' && window.history.length > 1) {
-            router.back();
-        } else {
-            router.push('/');
-        };
-    };
+    
 
     return (
         <div className="container w-full h-dvh mx-auto px-4">
             <div className="flex flex-col items-center justify-center h-full gap-10">
                 <_eyes/>
-                <div className='space-y-3 text-center'>
-                    <h1 className="uppercase font-bold text-3xl md:text-4xl lg:text-4xl text-(--custom-cream-yellow) flex gap-15">
-                        {error?.message ?? ''}
-                    </h1>
-                    <p className="font-bold text-lg md:text-2xl lg:text-3xl text-white">
-                        {status} error
-                    </p>
-                </div>
-                <button
-                    type="button" 
-                    className="text-xl py-3 px-5 text-black bg-(--custom-cream-yellow) hover:bg-(--custom-cream-yellow-darker) focus:ring-4 focus:ring-yellow-500 rounded-lg focus:outline-none"
-                    onClick={goBack}
-                >
-                    Back to Home
-                </button>
+                <_errorMsg error={error} status={status}/>
+                <_errorBtn/>
             </div>
         </div>
     );
---
+++ src/components/error/_errorMsg.js
@@ -0,0 +1,25 @@
+'use client';
+
+export default function _errorMsg({ status, error }) {
+    return (
+        <div className='space-y-3 text-center'>
+            {error?.message ? (
+                <h1 className="uppercase font-bold text-3xl md:text-4xl lg:text-4xl text-(--custom-cream-yellow) flex gap-15">
+                    {error?.message}
+                </h1>
+            ) : (
+                <h1 className="uppercase font-bold text-3xl md:text-4xl lg:text-4xl text-white flex gap-15">
+                    Oops! Something went wrong!
+                </h1>
+            )}
+
+            {status ? (
+                <p className="font-bold text-lg md:text-2xl lg:text-3xl text-white">
+                    {status ? `${status} error` : 'Error?'}
+                </p>
+            ) : null
+            }
+
+        </div>
+    );
+};
