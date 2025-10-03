68da843c0f3b384b8c22f3a898fbb423fc91ab88
Jacob Soh <developer.swr.2001@gmail.com>
2025-10-04 00:19:47 +0800

    Fixing useRouter?

--- src/components/error/_errorBtn.js
+++ src/components/error/_errorBtn.js
@@ -1,27 +1,29 @@
 "use client";
-import { useRouter } from 'next/navigation';
 
-export default function _errorBtn() {
+import { useRouter } from "next/navigation";
+
+export default function ErrorBtn() {
     const router = useRouter();
 
     const goBack = () => {
-        router.push('/');
+        router.push("/");
     };
+
     return (
         <button
             type="button"
-            className={`
+            className="
                 lg:text-xl lg:py-3 lg:px-5
                 md:text-md md:px-4
                 text-sm py-2 px-3
-                text-black bg-(--custom-cream-yellow) 
-                hover:bg-(--custom-cream-yellow-darker) 
-                focus:ring-4 focus:ring-yellow-500 
-                rounded-lg focus:outline-none    
-            `}
+                text-black bg-[var(--custom-cream-yellow)]
+                hover:bg-[var(--custom-cream-yellow-darker)]
+                focus:ring-4 focus:ring-yellow-500
+                rounded-lg focus:outline-none
+            "
             onClick={goBack}
         >
             Back to Home
         </button>
     );
-};
+}
