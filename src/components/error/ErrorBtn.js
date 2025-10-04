40dba39440749fe638f4a60fdc5ab6d70de8c655
Jacob Soh <developer.swr.2001@gmail.com>
2025-10-04 13:56:27 +0800

    Update Error Handling Page UI

--- src/components/error/ErrorBtn.js
+++ src/components/error/ErrorBtn.js
@@ -7,9 +7,9 @@ export default function ErrorBtn() {
     return (
         <Link
             className='
-                lg:text-xl lg:py-3 lg:px-5
-                md:text-md md:px-4
-                text-sm py-2 px-3
+                lg:text-lg text-sm
+                lg:py-3 py-2 
+                lg:px-5 md:px-4 px-3
                 text-black bg-[var(--custom-cream-yellow)]
                 hover:bg-[var(--custom-cream-yellow-darker)]
                 focus:ring-4 focus:ring-yellow-500
