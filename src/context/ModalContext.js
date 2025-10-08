0a0c11e131131136063d60a7ad27e8d6e87629bd
JacobSoh <138766971+JacobSoh@users.noreply.github.com>
2025-10-08 15:42:25 +0800

    Convert lr modal (#20)
    * Update form error handling
    
    * Changed auth form to modal
    
    * Update Navigation

---
+++ src/context/ModalContext.js
@@ -0,0 +1,36 @@
+'use client';
+
+import { createContext, useContext, useState, useCallback } from 'react';
+import Modal from '@/components/ModalComponent'; // your base modal (Dialog + backdrop)
+
+const ModalContext = createContext();
+
+export function ModalProvider({ children }) {
+    const [modal, setModal] = useState(null);
+
+    const openModal = useCallback(({content,title='',titleClassName="text-base font-semibold text-gray-100"}) => {
+        setModal({content, title, titleClassName});
+    }, []);
+
+    const closeModal = useCallback(() => {
+        setModal(null)
+    }, []);
+
+    return (
+        <ModalContext.Provider value={{ openModal, closeModal }}>
+            {children}
+
+            {modal?.content && (
+                <Modal isOpen={true} onClose={closeModal} title={modal.title} titleClassName={modal.titleClassName}>
+                    {typeof modal?.content === 'function' ? modal?.content() : modal?.content}
+                </Modal>
+            )}
+        </ModalContext.Provider>
+    );
+}
+
+export function useModal() {
+    const ctx = useContext(ModalContext);
+    if (!ctx) throw new Error('useModal must be used inside <ModalProvider>');
+    return ctx;
+}
