42f2ae168986c343f3e0677bedd8052eb2cb2e6b
Javen420 <Javent2003@gmail.com>
2025-10-30 16:05:14 +0800

    IDk

--- src/components/AuctionCard.js
+++ src/components/AuctionCard.js
@@ -175,7 +175,7 @@ export const BigAuctionCard = ({ name, description, picUrl, endTime, currentBid 
       </div>
 
       {/* Info Panel */}
-      <div className="absolute bottom-6 left-6 bg-purple-200/ backdrop-blur-sm rounded-xl p-6 text-white w-[90%] max-w-[600px]">
+      <div className="absolute bottom-6 left-6 bg-purple-200/ backdrop-blur-none rounded-xl p-6 text-white w-[90%] max-w-[600px]">
         <h2 className="text-4xl font-extrabold">{name}</h2>
         <p className="mt-2 text-lg text-purple-200 line-clamp-2">{description}</p>
         <div className="mt-4 flex justify-between items-center text-xl">
